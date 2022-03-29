602c0abaa65f89a97a2703035983eb5b6503a36c
viniciusdepaula1 <viniciusdepaula199@gmail>
2022-03-29 15:47:41 -0300

    reduce optimization

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,14 @@
-/**
- * All credits to https://github.com/cvb941/kotlin-parallel-operations
- * for the code base
- */
-
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.Default
 import kotlin.random.Random
 import kotlin.collections.*
 import kotlin.math.ceil
+import kotlin.math.pow
 import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
     val cpuCores: Int = Runtime.getRuntime().availableProcessors();
-    val randomValues = List(100) { Random.nextInt(0, 9999) }
+    val randomValues = List(999999) { Random.nextInt(0, 100) }
 
     val totalTimeParallel = measureTimeMillis {
         runBlocking {
@@ -31,22 +27,32 @@ fun main(args: Array<String>) {
     println("result without thread: ${randomValues.reduce { acc: Int, item: Int -> acc + item }}")
 }
 
+
 suspend fun <T> Iterable<T>.reduceParallel(
     chunkSize: Int,
+    itr: Int,
     operation: (T, T) -> T
 ): T = chunked(chunkSize).map { subChunk ->
     CoroutineScope(Default).async {
-        println("I am running on ${Thread.currentThread().name}")
+        //println("I am running on ${Thread.currentThread().name}")
         subChunk.reduce { acc, t -> operation(acc, t) }
     }
-}.map { it.await() }.reduce{ acc, t -> operation(acc, t) }
+}.map { it.await() }.reduceParallel(itr = itr + 1) { acc, t -> operation(acc, t) }
 
 suspend inline fun <T> Collection<T>.reduceParallel(
     chunksCount: Int = Runtime.getRuntime().availableProcessors(),
-    noinline operation: (T, T) -> T
+    itr: Int = 0,
+    noinline operation: (T, T) -> T,
 ): T {
+    if(size == 1) {
+        println(this.first())
+        return this.first()
+    }
+
     if (chunksCount <= 0)
         throw IllegalArgumentException("chunksCount must be a positive integer")
-    val chunkSize = ceil(size / chunksCount.toDouble()).toInt()
-    return asIterable().reduceParallel(chunkSize, operation)
+
+    val chunkSize = ceil(size / (chunksCount.toDouble()/ 2.0.pow(itr))).toInt()
+
+    return asIterable().reduceParallel(chunkSize, itr, operation)
 }
